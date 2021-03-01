8b1878549d85b8b31d7585faecec962be4840914
r-bota <76598683+r-bota@users.noreply.github.com>
2021-03-01 10:43:01 -0800

    repo created

---
+++ app.js
@@ -0,0 +1,39 @@
+var qwerty = document.getElementById("qwerty");
+var phrase = document.getElementById("phrase");
+var missed = 0;
+
+//start button to hide overlay
+overlay = document.getElementById("overlay");
+startButton = document.querySelector(".btn__reset");
+startButton.addEventListener("click", () => { overlay.style.display = "none" });
+
+//phrases for players to guess
+const phrases = ["first", "second", "third", "fourth", "fifth"]; 
+
+//this function forms an array in "phraseLetters" from the letters of a string randomly
+//selected from the array passed in as arr
+function getRandomPhraseAsArray(arr) { 
+    
+    var phraseArray = Array.from(arr[Math.floor(Math.random() * arr.length)]);
+    return phraseArray;
+    
+}
+
+var phraseArray = getRandomPhraseAsArray(phrases);
+
+//function to add the letters to the game screen
+function addPhraseToDisplay(arr) {
+    
+    for (let i = 0; i < arr.length; i++) {
+        const li = document.createElement("li")
+        var letters = document.createTextNode(arr[i]);
+        li.className = "letter";
+        li.appendChild(letters);
+        phrase.appendChild(li);
+    }
+   
+};
+ 
+function checkLetter() { }
+
+addPhraseToDisplay(phraseArray);
