774a011fd967bd7575ec351f7d8eccb263c538a5
r-bota <76598683+r-bota@users.noreply.github.com>
2021-03-02 10:06:21 -0800

    Added keyboard

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ startButton = document.querySelector(".btn__reset");
 startButton.addEventListener("click", () => { overlay.style.display = "none" });
 
 //phrases for players to guess
-const phrases = ["first", "second", "third", "fourth", "fifth"]; 
+const phrases = ["first", "second", "third", "fourth", "fifth"];
 
 //this function forms an array in "phraseLetters" from the letters of a string randomly
 //selected from the array passed in as arr
-function getRandomPhraseAsArray(arr) { 
-    
+function getRandomPhraseAsArray(arr) {
+
     var phraseArray = Array.from(arr[Math.floor(Math.random() * arr.length)]);
     return phraseArray;
-    
+
 }
 
 var phraseArray = getRandomPhraseAsArray(phrases);
 
 //function to add the letters to the game screen
 function addPhraseToDisplay(arr) {
-    
+
     for (let i = 0; i < arr.length; i++) {
         const li = document.createElement("li")
         var letters = document.createTextNode(arr[i]);
@@ -31,17 +31,33 @@ function addPhraseToDisplay(arr) {
         li.appendChild(letters);
         phrase.appendChild(li);
     }
-   
+
 };
- 
-function checkLetter(btn) { 
-    const letters = document.getElementsByClassName("letter")
+
+
+
+const keyboard = document.querySelectorAll(".keyrow button");
+keyboard.forEach(item => {
+    item.addEventListener("click", event => {
+        item.className = "chosen";
+        btn = item.textContent;
+        checkLetter(btn);
+    })
+})
+
+
+function checkLetter(btn) {
+    const letters = document.getElementsByClassName("letter");
     for (let i = 0; i < letters.length; i++) {
-        if (btn === letters[i]) {
-            
+        if (btn === letters[i].textContent) {
+            console.log(btn)
+            letters[i].className += " show"
         }
     }
 
+ 
 }
+
+
 
 addPhraseToDisplay(phraseArray);
