ef095c28c3f7e12312c8681a7dadd212bb806d2f
r-bota <76598683+r-bota@users.noreply.github.com>
2021-03-05 13:58:15 -0800

    added reset on win on lose

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ var qwerty = document.getElementById("qwerty");
 var phrase = document.getElementById("phrase");
 var missed = 0;
 var hearts = document.querySelectorAll(".tries img");
+var keyboard = document.querySelectorAll(".keyrow button");
+var letterChecker = document.querySelectorAll(".letter") + document.querySelectorAll(".space");
+var winN = null
 
-
-//start button to hide overlay
+//start button to hide overlay and reset gameboard
 overlay = document.getElementById("overlay");
 startButton = document.querySelector(".btn__reset");
-startButton.addEventListener("click", () => {
+startButton.addEventListener("click", function () {
     overlay.style.display = "none"
-    
+    reset()
+    listen()
 });
 
 
@@ -27,6 +30,7 @@ var phraseArray = getRandomPhraseAsArray(phrases);
 
 //function to add the letters to the game screen
 function addPhraseToDisplay(arr) {
+
     for (let i = 0; i < arr.length; i++) {
         const li = document.createElement("li")
         var letters = document.createTextNode(arr[i]);
@@ -43,20 +47,26 @@ function addPhraseToDisplay(arr) {
 addPhraseToDisplay(phraseArray);
 
 
-
-const keyboard = document.querySelectorAll(".keyrow button");
-for (let i = 0; i < keyboard.length; i++) {
-    keyboard[i].addEventListener("click", function () {
-        keyboard[i].className = "chosen";
-        keyboard[i].disabled = "true";
-        btn = keyboard[i].textContent;
-        checkLetter(btn);
-        checkWin()
-        if (checkLetter(btn) !== btn) {
-            missed += 1;
-            hearts[missed - 1].src = "images/lostHeart.png";
+function listen() {
+    for (let i = 0; i < keyboard.length; i++) {
+        keyboard[i].addEventListener("click", function () {
+            keyboard[i].className = "chosen";
+            keyboard[i].disabled = true;
+            btn = keyboard[i].textContent;
+            checkLetter(btn);
+            if (checkLetter(btn) !== btn) {
+                missed += 1;
+                hearts[missed - 1].src = "images/lostHeart.png";
+            }
+            checkWin()
+        });
+        if (winN) {
+            return (console.log("test"))
+        } else if (winN === false) {
+            return (console.log("test20"))
         }
-    });
+    }
+
 }
 
 
@@ -65,7 +75,7 @@ function checkLetter(btn) {
     var match = null;
     for (let i = 0; i < letters.length; i++) {
         if (btn === letters[i].textContent) {
-            letters[i].className = "letter show";
+            letters[i].classList.add("show");
             match = btn;
         }
     }
@@ -76,23 +86,53 @@ function checkWin() {
     var liLetter = document.getElementsByClassName("letter");
     var liShow = document.getElementsByClassName("show");
     if (liLetter.length === liShow.length) {
-        overlay.className = "win";
-        overlay.style.display = "flex";
-        reset()
+        win()
+        
     }
-    if (missed === 4) {
-        overlay.className = "lose";
-        overlay.style.display = "flex";
-        reset()
+    if (missed === 5) {
+        lose()
+        
     }
 }
 
+function win() {
+    overlay.className = "win";
+    overlay.style.display = "flex";
+    startButton.textContent = "Reset";
+    winN = true;
+}
+
+function lose() {
+    overlay.className = "lose";
+    overlay.style.display = "flex";
+    startButton.textContent = "Reset";
+    winN = false;
+}
+
 function reset() {
-    if (overlay.className = "win") {
-        startButton.textContent = "Reset";
-        startButton.addEventListener("click", function () {
-            missed = 0;
-            
-        })
+    missed = 0;
+    for (let i = 0; i < hearts.length; i++) {
+        hearts[i].src = "images/liveHeart.png"
+    }
+    for (let i = 0; i < keyboard.length; i++) {
+        keyboard[i].classList.remove("chosen");
+        keyboard[i].disabled = false;
     }
 }
+
+
+// function reset() {
+//     if (overlay.className === "lose") {
+//         startButton.textContent = "Reset";
+//         startButton.addEventListener("click", function () {
+
+//             for (let i = 0; i < hearts.length; i++) {
+//                 hearts[i].src = "images/liveHeart.png"
+//             }
+//             for (let i = 0; i < keyboard.length; i++) {
+//                 keyboard[i].classList.remove("chosen");
+
+//             }
+//         })
+//     }  listen()
+// }
